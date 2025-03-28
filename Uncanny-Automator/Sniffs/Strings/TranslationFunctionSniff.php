dc85d8eb6be84084fed126cee4f7e2a2e56f62d7
Saad Siddique <saad@uncannyowl.com>
2025-03-28 17:54:44 -0400

    Update TranslationFunctionSniff.php

--- Uncanny-Automator/Sniffs/Strings/TranslationFunctionSniff.php
+++ Uncanny-Automator/Sniffs/Strings/TranslationFunctionSniff.php
@@ -21,6 +21,7 @@ class TranslationFunctionSniff implements Sniff {
 	private $non_context_functions = array(
 		'__',
 		'_e',
+		'_x',
 		'esc_html__',
 		'esc_html_e',
 		'esc_attr__',
@@ -61,7 +62,7 @@ class TranslationFunctionSniff implements Sniff {
 		// Check for unescaped translation functions
 		if (in_array($function_name, $this->unescaped_functions, true)) {
 			$phpcs_file->addError(
-				'Translation function should use esc_html__, esc_attr__, or _x for proper escaping and context',
+				'Translation function should use esc_html__(), esc_attr__(), or esc_html_x() for proper escaping and context',
 				$stack_ptr,
 				'UnescapedTranslation'
 			);
@@ -71,9 +72,9 @@ class TranslationFunctionSniff implements Sniff {
 		$filename = $phpcs_file->getFilename();
 		if (false !== strpos($filename, '/src/integrations/')) {
 			if (in_array($function_name, $this->non_context_functions, true)) {
-				$context_alternative = $this->get_context_alternative($function_name);
+				$context_alternative = $this->get_context_alternative($function_name, $phpcs_file);
 				$phpcs_file->addError(
-					'Use %s() with context instead of %s() in integration strings. This helps translators better understand the context of the string. Choose the appropriate escaping function (esc_html_x, esc_attr_x, etc.) based on the content type.',
+					'Use %s() with context instead of %s() in integration strings. This helps translators better understand the context of the string. Choose the appropriate escaping function (esc_html_x(), esc_attr_x(), etc.) based on the content type.',
 					$stack_ptr,
 					'NoContext',
 					array($context_alternative, $function_name)
@@ -86,24 +87,40 @@ class TranslationFunctionSniff implements Sniff {
 	 * Get the recommended context function alternative.
 	 *
 	 * @param string $function The current function name.
+	 * @param File   $phpcs_file The PHP_CodeSniffer file where the token was found.
 	 * @return string The recommended context function.
 	 */
-	private function get_context_alternative($function): string {
-		switch ($function) {
-			case '__':
-				return '_x';
-			case '_e':
-				return '_ex';
-			case 'esc_html__':
-				return 'esc_html_x';
-			case 'esc_html_e':
-				return 'esc_html_x';
-			case 'esc_attr__':
-				return 'esc_attr_x';
-			case 'esc_attr_e':
-				return 'esc_attr_x';
-			default:
-				return '_x';
+	private function get_context_alternative($function, File $phpcs_file): string {
+		$filename = $phpcs_file->getFilename();
+		$is_integration = false !== strpos($filename, '/src/integrations/');
+
+		if ($is_integration) {
+			switch ($function) {
+				case '_e':
+				case 'esc_html_e':
+					return 'echo esc_html_x';
+				case 'esc_attr__':
+					return 'esc_attr_x';
+				case 'esc_attr_e':
+					return 'echo esc_attr_x';
+				case 'esc_html__':
+				case '__':
+				case '_x':
+					return 'esc_html_x';
+				default:
+					return 'esc_html_x';
+			}
+		} else {
+			switch ($function) {
+				case '__':
+					return 'esc_html__';
+				case '_e':
+					return 'esc_html_e';
+				case '_x':
+					return 'esc_html_x';
+				default:
+					return 'esc_html__';
+			}
 		}
 	}
 } 
